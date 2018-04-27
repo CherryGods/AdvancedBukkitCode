83c527481ecd658329ccb3f3c89c984d3398be88
CherryGods <the_lty_mail@foxmail.com>
2018-04-27 16:39:30 +0800

    CD-冷却时间

---
+++ 01_CoolDowns/src/org/cherrygods/main/CoolDownsClass.java
@@ -0,0 +1,19 @@
+package org.cherrygods.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author CherrGods
+ * @since 2018-4-27 16:37:00
+ */
+public class CoolDownsClass extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
