90379456599f39dab79db27ec4b21b382990aa39
e <the_lty_mail@foxmail.com>
2018-04-30 23:21:08 +0800

    自定义...xxx

---
+++ 04_CustomeEnchants/src/org/cherrygods/main/CustomeEnchatsMain.java
@@ -0,0 +1,11 @@
+package org.cherrygods.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author CherryGods
+ * @since  2018-4-30 23:20:10
+ */
+public class CustomeEnchatsMain extends JavaPlugin {
+
+}
