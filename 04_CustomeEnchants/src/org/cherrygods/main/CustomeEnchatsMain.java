e32a89124cd37c120bc4f6aad2b2e1b161837f0b
e <the_lty_mail@foxmail.com>
2018-04-30 23:24:07 +0800

    从配置文件随机读取一个物品，并且在玩家进入服务器时，放入玩家背包

--- 04_CustomeEnchants/src/org/cherrygods/main/CustomeEnchatsMain.java
+++ 04_CustomeEnchants/src/org/cherrygods/main/CustomeEnchatsMain.java
@@ -1,5 +1,6 @@
 package org.cherrygods.main;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -7,5 +8,17 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @since  2018-4-30 23:20:10
  */
 public class CustomeEnchatsMain extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage("\n\n"+ChatColor.GREEN+getName()+" >> "
+                +ChatColor.AQUA+"has been Disable!");
+        super.onDisable();
+    }
 
+    @Override
+    public void onEnable() {
+        getServer().getConsoleSender().sendMessage("\n\n"+ChatColor.GREEN+getName()+" >> "
+                +ChatColor.AQUA+"has been Enable!");
+        super.onEnable();
+    }
 }
